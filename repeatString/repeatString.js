55ed6dd939566e0afef4cc3e9e0f0099043ac916
Karina Rodriguez <Karina.Rodriguez.TH@dartmouth.edu>
2021-06-30 21:23:29 -0400

    ability to repeat strings for a positive amount of times

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, amtToRepeat) {
+    let newString = '';
 
+    // Verify amount to repeat string by is positive
+    if (amtToRepeat < 0) {
+        return 'ERROR';
+    }
+
+    for (let i = 0; i < amtToRepeat; i++) {
+        newString += string;
+    }
+    return newString;
 };
 
 module.exports = repeatString;
