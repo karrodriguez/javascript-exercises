d0d20fa61ecb3f1ba221d481ef33a5b954a7da94
Karina Rodriguez <Karina.Rodriguez.TH@dartmouth.edu>
2021-07-01 14:40:47 -0400

    added function that can determine whether any year is a leap year

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    year = +year;  //convert to 'number'
+    return (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) ? true : false;
 };
 
 module.exports = leapYears;
