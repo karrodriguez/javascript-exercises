29fc2f923106035dc51a8af641a4a4be7e9656f8
Karina Rodriguez <Karina.Rodriguez.TH@dartmouth.edu>
2021-07-01 14:27:49 -0400

    ability to sum between any two positive integers

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let sum = 0;
+    if (typeof(int1) !== 'number' || typeof(int2) !== 'number') {
+        return 'ERROR';
+    }
+    else if (int1 < 0 || int2 < 0) {
+        return 'ERROR'
+    }
 
+    if (int2 < int1) {
+        [int1, int2] = [int2, int1];
+    }
+
+    for (let i = int1; i <= int2; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 module.exports = sumAll;
