f057180efd10669817bd4dfd2f88614564ed814c
Karina Rodriguez <Karina.Rodriguez.TH@dartmouth.edu>
2021-07-01 13:54:59 -0400

    added ability to remove any number of specified values from array

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let args = [].slice.call(arguments);
+    args.shift();
 
+    let newArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (!(args.includes(array[i]))) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
 
 module.exports = removeFromArray;
