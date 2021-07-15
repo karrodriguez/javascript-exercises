950657508abd0d3141a0a3a49663e8c15a9dd40a
Karina Rodriguez <Karina.Rodriguez.TH@dartmouth.edu>
2021-07-14 23:25:57 -0400

    completed assignment, i.e., created calculator with add, subtract, sum, multiply, and factorial capabilities

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,26 +1,36 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  return array.reduce((total, currentVal) => total + currentVal, 0);
 	
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.reduce((total, currentVal) => total * currentVal);
 
 };
 
-const power = function() {
+const power = function(a, b) {
+  return a**b;
 	
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(a) {
+  if (a == 0) {
+    return 1;
+  }
+  return a * factorial(a - 1);
+}
+
+console.log(factorial(10));
 
 module.exports = {
   add,
@@ -30,3 +40,5 @@ module.exports = {
   power,
   factorial
 };
+
+
