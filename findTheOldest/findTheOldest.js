b34c2c2173f38835cbf9db61283bae738e42262c
Karina Rodriguez <Karina.Rodriguez.TH@dartmouth.edu>
2021-07-15 12:58:08 -0400

    completed assignment using the sort method

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldestPerson = people.sort(function(personA, personB) {
+        const currentYear = new Date().getFullYear();
 
+        let prevPersonAge = ("yearOfDeath" in personA) ? personA.yearOfDeath - personA.yearOfBirth : currentYear - personA.yearOfBirth;
+        let currPersonAge = ("yearOfDeath" in personB) ? personB.yearOfDeath - personB.yearOfBirth : currentYear - personB.yearOfBirth;
+
+        return (prevPersonAge > currPersonAge) ? -1: 1;
+    });
+
+    return oldestPerson[0];
 };
 
 module.exports = findTheOldest;
