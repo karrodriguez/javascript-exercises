3892529bc62fc581fb577e80c7eb4ff6d0fa4970
Karina Rodriguez <Karina.Rodriguez.TH@dartmouth.edu>
2021-07-15 12:17:11 -0400

    completed assignment using recursion

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+let fibonacci = function(n) {
+    if (n == 0) {
+        return 0;
+    }
+    else if (n == 1) {
+        return 1;
+    }
+    else if (n >= 2) {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+    else {
+        return "OOPS";
+    }
 };
 
 module.exports = fibonacci;
